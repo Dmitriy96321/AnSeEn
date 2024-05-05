c5b071906065263b923261ce2f0623017d207054
Dmitriy96321 <xaptmah2010@yandex.ru>
2024-05-05 18:44:47 +0200

    wb.04.03

--- src/main/java/searchengine/dto/search/SearchResponse.java
+++ src/main/java/searchengine/dto/search/SearchResponse.java
@@ -1,9 +1,11 @@
 package searchengine.dto.search;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SearchResponse {
 }
