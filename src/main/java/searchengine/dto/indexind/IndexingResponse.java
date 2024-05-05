aa6d29b9d898432391d45046bb8f94d0b332551d
Dmitriy96321 <xaptmah2010@yandex.ru>
2024-05-05 18:39:56 +0200

    wb.04.01

--- src/main/java/searchengine/dto/indexind/IndexingResponse.java
+++ src/main/java/searchengine/dto/indexind/IndexingResponse.java
@@ -1,10 +1,13 @@
 package searchengine.dto.indexind;
 
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 @Builder
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class IndexingResponse {
 private boolean result;
 private String error;
---
+++ src/main/java/searchengine/dto/search/SearchResponse.java
@@ -0,0 +1,9 @@
+package searchengine.dto.search;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class SearchResponse {
+}
